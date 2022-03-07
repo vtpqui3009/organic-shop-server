54510c096defb9d8aabedd3839e959362094b6e7
vtpq <90906759+vtpqui3009@users.noreply.github.com>
2022-03-07 20:13:16 +0700

    let's go

--- config/database.js
+++ config/database.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose.connect(process.env.DB_URI).then((data) => {
-    console.log(`mongodb connected with server: ${data.connection.host}`);
-  });
+  mongoose
+    .connect(
+      "mongodb+srv://useruser:useruser@cluster0.oevef.mongodb.net/orchid?retryWrites=true&w=majority"
+    )
+    .then((data) => {
+      console.log(`mongodb connected with server: ${data.connection.host}`);
+    });
 };
 module.exports = connectDatabase;

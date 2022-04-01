de9344b67f457313924faad3fb2514a8c9310552
vtpq <90906759+vtpqui3009@users.noreply.github.com>
2022-04-01 11:40:03 +0700

    Fix Url

--- config/database.js
+++ config/database.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose
-    .connect(
-      "mongodb+srv://useruser:useruser@cluster0.oevef.mongodb.net/orchid?retryWrites=true&w=majority"
-    )
-    .then((data) => {
-      console.log(`mongodb connected with server: ${data.connection.host}`);
-    });
+  // mongoose
+  //   .connect(
+  //     "mongodb+srv://useruser:useruser@cluster0.oevef.mongodb.net/orchid?retryWrites=true&w=majority"
+  //   )
+  //   .then((data) => {
+  //     console.log(`mongodb connected with server: ${data.connection.host}`);
+  //   });
+  mongoose.connect(process.env.DB_URI).then((data) => {
+    console.log(`mongodb connected with server: ${data.connection.host}`);
+  });
 };
 module.exports = connectDatabase;
